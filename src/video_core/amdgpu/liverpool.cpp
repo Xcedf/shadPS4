d6a0057e6fd527306ac6ddb2823f6ac6fddf07bd
ZEROx <Autoran1@yandex.ru>
2025-08-14 16:08:51 +0300

    hacks

--- src/video_core/amdgpu/liverpool.cpp
+++ src/video_core/amdgpu/liverpool.cpp
@@ -607,7 +607,7 @@ Liverpool::Task Liverpool::ProcessGraphics(std::span<const u32> dcb, std::span<c
                     // immediately
                     regs.cp_strmout_cntl.offset_update_done = 1;
                 } else if (event->event_index.Value() == EventIndex::ZpassDone) {
-                    LOG_WARNING(Render, "Unimplemented occlusion query");
+                    LOG_DEBUG(Render, "Unimplemented occlusion query");
                 }
                 break;
             }
@@ -858,7 +858,7 @@ Liverpool::Task Liverpool::ProcessCompute(std::span<const u32> acb, u32 vqid) {
             break;
         }
 
-        if (header->type == 2) {
+        /*if (header->type == 2) {
             // Type-2 packet are used for padding purposes
             next_dw_off = 1;
             acb = NextPacket(acb, next_dw_off);
@@ -867,9 +867,10 @@ Liverpool::Task Liverpool::ProcessCompute(std::span<const u32> acb, u32 vqid) {
                 *queue.read_addr %= queue.ring_size_dw;
             }
             continue;
-        }
+        }*/
 
         if (header->type != 3) {
+            continue;
             // No other types of packets were spotted so far
             UNREACHABLE_MSG("Invalid PM4 type {}", header->type.Value());
         }
