04dc3a499d56e8c60f237fd3a545c99ca7f1701c
ZEROx <Autoran1@yandex.ru>
2025-11-25 23:20:37 +0300

    Revert "buffer_cache: Split DMA fault handling code from buffer cache (#3809)"
    This reverts commit 3f86c2e94ad645d8a26b676b026b39fb1f2b8071.
    
    # Conflicts:
    #	src/video_core/buffer_cache/buffer_cache.cpp

--- src/video_core/renderer_vulkan/vk_scheduler.cpp
+++ src/video_core/renderer_vulkan/vk_scheduler.cpp
@@ -87,6 +87,15 @@ void Scheduler::Wait(u64 tick) {
         Flush(info);
     }
     master_semaphore.Wait(tick);
+
+    // CAUTION: This can introduce unexpected variation in the wait time.
+    // We don't currently sync the GPU, and some games are very sensitive to this.
+    // If this becomes a problem, it can be commented out.
+    // Idealy we would implement proper gpu sync.
+    while (!pending_ops.empty() && pending_ops.front().gpu_tick <= tick) {
+        pending_ops.front().callback();
+        pending_ops.pop();
+    }
 }
 
 void Scheduler::PopPendingOperations() {
@@ -103,7 +112,9 @@ void Scheduler::AllocateWorkerCommandBuffers() {
     };
 
     current_cmdbuf = command_pool.Commit();
-    Check(current_cmdbuf.begin(begin_info));
+    auto begin_result = current_cmdbuf.begin(begin_info);
+    ASSERT_MSG(begin_result == vk::Result::eSuccess, "Failed to begin command buffer: {}",
+               vk::to_string(begin_result));
 
     // Invalidate dynamic state so it gets applied to the new command buffer.
     dynamic_state.Invalidate();
@@ -131,7 +142,9 @@ void Scheduler::SubmitExecution(SubmitInfo& info) {
 #endif
 
     EndRendering();
-    Check(current_cmdbuf.end());
+    auto end_result = current_cmdbuf.end();
+    ASSERT_MSG(end_result == vk::Result::eSuccess, "Failed to end command buffer: {}",
+               vk::to_string(end_result));
 
     const vk::Semaphore timeline = master_semaphore.Handle();
     info.AddSignal(timeline, signal_value);
