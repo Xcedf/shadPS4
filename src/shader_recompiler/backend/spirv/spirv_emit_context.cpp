5308d343063fd88458e67c55cb3735a4c151d84c
ZEROx <Autoran1@yandex.ru>
2026-09-12 01:14:35 +0400

    mem sparse old 2

--- src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
+++ src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
@@ -789,12 +789,6 @@ EmitContext::BufferSpv EmitContext::DefineBuffer(bool is_storage, bool is_writte
     case BufferType::Flatbuf:
         Name(id, "srt_flatbuf");
         break;
-    case BufferType::BdaPagetable:
-        Name(id, "bda_pagetable");
-        break;
-    case BufferType::FaultBuffer:
-        Name(id, "fault_buffer");
-        break;
     case BufferType::SharedMemory:
         Name(id, "ssbo_shmem");
         break;
@@ -814,10 +808,6 @@ void EmitContext::DefineBuffers() {
         // Set indexes for special buffers.
         if (desc.buffer_type == BufferType::Flatbuf) {
             flatbuf_index = buffers.size();
-        } else if (desc.buffer_type == BufferType::BdaPagetable) {
-            bda_pagetable_index = buffers.size();
-        } else if (desc.buffer_type == BufferType::FaultBuffer) {
-            fault_buffer_index = buffers.size();
         }
 
         // Define aliases depending on the shader usage.
@@ -1150,7 +1140,7 @@ Id EmitContext::DefineUfloatM5ToFloat32(u32 mantissa_bits, const std::string_vie
 }
 
 Id EmitContext::DefineGetBdaPointer() {
-    const auto caching_pagebits{
+    /*const auto caching_pagebits{
         Constant(U64, static_cast<u64>(VideoCore::BufferCache::CACHING_PAGEBITS))};
     const auto caching_pagemask{Constant(U64, VideoCore::BufferCache::CACHING_PAGESIZE - 1)};
 
@@ -1205,7 +1195,8 @@ Id EmitContext::DefineGetBdaPointer() {
     const auto result{OpPhi(U64, addr, available_label, fallback_result, fault_label)};
     OpReturnValue(result);
     OpFunctionEnd();
-    return func;
+    return func;*/
+    UNREACHABLE();
 }
 
 Id EmitContext::DefineReadConst(bool dynamic) {
